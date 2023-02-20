2e32a4614155e7a9d7bc78914a34ace7300c2cfc
rodrigo.resque <rodrigo.resque@mkdata.com.br>
2023-02-20 01:59:50 -0300

    updt

--- convert-fahrenheit-celsius-for.c
+++ convert-fahrenheit-celsius-for.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
 	
 	int fahr;
 
-	for(fahr = 300; fahr >= 0; fahr = fahr - 20) 
+	for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) 
 		printf("%3d %6.1f\n", fahr, (0.5/9.0) * (fahr - 32));
 
 }
